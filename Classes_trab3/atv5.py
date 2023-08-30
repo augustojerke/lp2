378849f8004236e0fcc58eed42c74c1451d9bc6e
Augusto <augustowieles@gmail.com>
2023-08-30 19:40:37 -0300

    asdkaks

--- Classes_trab3/atv5.py
+++ Classes_trab3/atv5.py
@@ -4,18 +4,27 @@ class Carro:
         self.distPercorrida = dist
     
     def abastecerCarro(self, abastecimento):
-        if self.gasolina == 50: print("Tanque Cheio!")
-        elif (self.gasolina + abastecimento) < 50: self.gasolina += abastecimento
-        else: self.gasolina = 50
+        if self.gasolina == 50:
+            print("Tanque Cheio!")
+        elif (self.gasolina + abastecimento) < 50:
+            self.gasolina += abastecimento
+        else:
+            self.gasolina = 50
     
-    def moverCarro(self, dist): self.distPercorrida += dist
+    def moverCarro(self, dist):
+        self.distPercorrida += dist
 
-    def getGasolina(self): return self.gasolina
-    def getDistancia(self): return self.distPercorrida
+    def getGasolina(self):
+        return self.gasolina
+    
+    def getDistancia(self):
+        return self.distPercorrida
 
-#Programa
+# Programa
 
 c1 = Carro(20, 200)
 c2 = Carro(30, 400)
-print(f"Carro 1 -  Distancia: {c1.distPercorrida}km , Gasolina Restante: {c1.distPercorrida/c1.gasolina} ")
-print(f"Carro 2 -  Distancia: {c2.distPercorrida}km , Gasolina Restante: {c2.distPercorrida/c2.gasolina} ")
+gasolina_restante_c1 = c1.getGasolina() - (c1.getDistancia() / 15)
+gasolina_restante_c2 = c2.getGasolina() - (c2.getDistancia() / 15)
+print(f"Carro 1 - Distancia: {c1.getDistancia()} km, Gasolina Restante: {gasolina_restante_c1:.2f} litros")
+print(f"Carro 2 - Distancia: {c2.getDistancia()} km, Gasolina Restante: {gasolina_restante_c2:.2f} litros")
