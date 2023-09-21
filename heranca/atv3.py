aae388608a5741efee8c1fe67ef614f24f3aefb6
Augusto <augustowieles@gmail.com>
2023-09-21 19:43:06 -0300

    herenca

---
+++ heranca/atv3.py
@@ -0,0 +1,25 @@
+class Pessoa():
+    def __init__(self, nome, endereco):
+        self.nome = nome
+        self.endereco = endereco
+
+    def getNome(self): return self.nome
+    def getEndereco(self): return self.endereco
+    def setNome(self, nome): self.nome = nome
+    def setEndereco(self, endereco): self.endereco = endereco
+
+class Empregado(Pessoa):
+    def __init__(self, nome, endereco, salarioBase, mesesTrabalhados):
+        super().__init__(nome, endereco)
+        self.salarioBase = salarioBase
+        self.mesesTrabalhados = mesesTrabalhados
+
+    def calcularSalario(self): return self.salarioBase * self.mesesTrabalhados
+    
+empregado = Empregado("Guto", "Rua Marechal, 123", 1500, 6)
+
+print(f"Nome do empregado: {empregado.getNome()}")
+print(f"Endereço do empregado: {empregado.getEndereco()}")
+print(f"Salário Base: {empregado.salarioBase}")
+print(f"Meses Trablhados: {empregado.mesesTrabalhados}")
+print(f"Salário: {empregado.calcularSalario()}")
